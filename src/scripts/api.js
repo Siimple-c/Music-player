76691366e44e25f422d14956d30197f487cda568
Your Name <you@example.com>
2023-01-19 22:13:49 +0100

    src/

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,30 +1,30 @@
 let songs = [
   {
     artiste: 'FKJ',
-    path:  "../src/assets/music/FKJ - Us.m4a",
+    path:  "../assets/music/FKJ - Us.m4a",
     title:'Us',
-    smallCover: "../src/assets/pictures/FKJ.PNG" ,
-    bigCover: "../src/assets/pictures/fkj-big.jpg",
+    smallCover: "../assets/pictures/FKJ.PNG" ,
+    bigCover: "../assets/pictures/fkj-big.jpg",
   },
   {
     artiste: 'Eminem',
-    path: "../src/assets/music/Eminem - Venom.m4a",
+    path: "../assets/music/Eminem - Venom.m4a",
     title:'Venom',
-    smallCover: "../src/assets/pictures/Eminem.PNG",
-    bigCover: "../src/assets/pictures/eminem-big.jpg",
+    smallCover: "../assets/pictures/Eminem.PNG",
+    bigCover: "../assets/pictures/eminem-big.jpg",
   },
   {
     artiste: 'Nina Simone',
-    path: "../src/assets/music/Nina Simone - Feeling Good.m4a",
+    path: "../assets/music/Nina Simone - Feeling Good.m4a",
     title:'Feeling Good',
-    smallCover: "../src/assets/pictures/Nina Simone.PNG" ,
-    bigCover: "../src/assets/pictures/nina-simone-big.jpg ",
+    smallCover: "../assets/pictures/Nina Simone.PNG" ,
+    bigCover: "../assets/pictures/nina-simone-big.jpg ",
   },
   {
     artiste: 'Bob Marley',
-    path: '../src/assets/music/Bob Marley - Is This Love.m4a',
+    path: '../assets/music/Bob Marley - Is This Love.m4a',
     title:'Is this love',
-    smallCover: "../src/assets/pictures/Bob-Marley.png ",
-    bigCover: "../src/assets/pictures/bob-marley-big.png",
+    smallCover: "../assets/pictures/Bob-Marley.png ",
+    bigCover: "../assets/pictures/bob-marley-big.png",
   }
 ]
